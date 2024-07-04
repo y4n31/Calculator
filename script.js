820980943887a127d8f655416b1075d2622e9796
y4n31 <91342617+y4n31@users.noreply.github.com>
2024-07-03 21:55:05 -0500

    created funcs operations and function operate()

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+let firstNum, secondNum, operator;
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    };
+};
